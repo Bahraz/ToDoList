352a5ec0dde185b80ad11b0c9cb2d50dd7c16a95
Bahraz <jarekg0626@gmail.com>
2025-07-09 16:50:47 +0200

    feat: add Bootstrap, favicon and environment variable support

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,6 +7,6 @@ class HomeController extends Controller
 {
     public function index(): void
     {
-        $this->view('home/index', ['title' => 'Witamy w SettlersWars!']);
+        $this->view('home/index', ['title' => 'Witamy w ToDoList!']);
     }
 }
