2aedb455a46ecf2e667a406db790866a8dbf2fd7
Bahraz <jarekg0626@gmail.com>
2025-07-09 16:53:47 +0200

    feat: add Bootstrap, favicon and environment variable support

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,6 +1,6 @@
 <p>To jest przykładowa strona ToDoList.</p>
 <div class="container mt-5">
   <div class="alert alert-success">
-    Działa, Bootstrap też działa 🎉
+    Działa, Bootstrap też działa !!! 🎉
   </div>
 </div>
