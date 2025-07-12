c98a83f9c27d73d31199be1df2cdc5bae8e84a5f
Bahraz <jarekg0626@gmail.com>
2025-07-12 13:33:10 +0200

    Add Bootstrap integration, styled footer, and partial navbar

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -9,11 +9,58 @@
     <meta name="author" content="Bahraz" />
 
     <title><?= htmlspecialchars($title ?? 'ToDoList') ?></title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/style.css" />
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-  </head>
+</head>
 <body>
 <header>
-    <h1><?= htmlspecialchars($title ?? 'ToDoList') ?></h1>
+    <div class="container">
+                                    <div class="btn-group">
+                                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Danger
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                                </ul>
+                            </div>
+            <h1><?= htmlspecialchars($title ?? 'ToDoList') ?></h1>
+        
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="/"> LOGO </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Strona Główna</a>
+                        </li>
+                        <li class="nav-item">
+
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="/home/index">Dodaj zadanie</a>
+                        </li>  
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home/index">Dzisiejsze zadania</a>
+                        </li>           
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about">Wszystkie zadania</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/contact">O Aplikacji</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/contact">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    </div>  
 </header>
 <main>
