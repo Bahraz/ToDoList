82f0ba0586f5c8afb3b5aa5e896355c402ac4a06
Bahraz <jarekg0626@gmail.com>
2025-08-02 16:17:33 +0200

    add: PDO Exception and RuntimeException to Model

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -1,27 +1,10 @@
 <?php
 namespace Bahraz\ToDoApp\Controllers;
 
-use Bahraz\ToDoApp\Models\Task;
 
 class AppController extends BaseController
 {
-    private function fetchApi(string $url): ?array
-    {
-        $curlHandle = curl_init();
-        curl_setopt($curlHandle, CURLOPT_URL, $url);
-        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlHandle, CURLOPT_TIMEOUT, 5); // limit czasu, opcjonalnie
-
-        $response = curl_exec($curlHandle);
-        $httpCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
-        curl_close($curlHandle);
-
-        if ($httpCode === 200 && $response !== false) {
-            return json_decode($response, true);
-        }
-    return null;
-    }
-
+    
     public function index(): void
     {
         $this->render('home/index');
