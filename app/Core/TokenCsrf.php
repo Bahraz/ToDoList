2dcee6c1141ee6a4141687aa8ec5bfb9e9df6341
Jaroslaw <jarekg0626@gmail.com>
2025-09-14 13:42:28 +0200

    rebuild: csrf token

--- app/Core/TokenCsrf.php
+++ app/Core/TokenCsrf.php
@@ -4,25 +4,63 @@ namespace Bahraz\ToDoApp\Core;
 
 class TokenCsrf
 {
+    private const SESSION_KEY = '_csrf_token';
+    private const TOKEN_EXPIRY = 3600; 
+
     public static function generateCsrf(): string
     {
-        if (session_status() === PHP_SESSION_NONE) {
+        if(session_status() !== PHP_SESSION_ACTIVE)
+        {
             session_start();
         }
 
-        if (empty($_SESSION['TokenCsrf'])) {
-            $_SESSION['TokenCsrf'] = bin2hex(random_bytes(32));
-        }
+        $token = bin2hex(random_bytes(32));
+        $_SESSION[self::SESSION_KEY] = [
+            'value' => $token,
+            'time' => time() + self::TOKEN_EXPIRY
+        ];
 
-        return $_SESSION['TokenCsrf'];
+        return $token;
     }
 
-    public static function validateCsrf(?string $token): bool
+    public static function getCsrf(): string
     {
-        if (session_status() === PHP_SESSION_NONE) {
+        if(session_status() !== PHP_SESSION_ACTIVE)
+        {
             session_start();
         }
 
-        return isset($_SESSION['TokenCsrf']) && hash_equals($_SESSION['TokenCsrf'], $token ?? '');
+        if(!isset($_SESSION[self::SESSION_KEY]) || self::expiredCsrf())
+        {
+            return self::generateCsrf();
+        }
+
+        return $_SESSION[self::SESSION_KEY]['value'];
+    }
+
+    public static function expiredCsrf(): bool
+    {
+        return (time() - $_SESSION[self::SESSION_KEY]['time']) > self::TOKEN_EXPIRY;
+    }
+
+    public static function verifyCsrf(?string $token): bool
+    {
+        if(session_status() !== PHP_SESSION_ACTIVE)
+        {
+            session_start();
+        }
+        
+        if(!$token || !isset($_SESSION[self::SESSION_KEY]))
+        {
+            return false;
+        }
+
+        if (self::expiredCsrf())
+        {
+            unset($_SESSION[self::SESSION_KEY]);
+            return false;
+        }
+
+        return hash_equals($_SESSION[self::SESSION_KEY]['value'], $token);
     }
 }
